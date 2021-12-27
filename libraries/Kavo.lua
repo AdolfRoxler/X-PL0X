acf4bac26ef7eaeffa5f1d751f70ba40d67b6926
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2021-12-27 21:09:37 +0100

    yo guys got the gui figured out

--- libraries/Kavo.lua
+++ libraries/Kavo.lua
@@ -226,6 +226,7 @@ function Kavo.CreateLib(kavName, themeList)
     ScreenGui.Name = LibName
     ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     ScreenGui.ResetOnSpawn = false
+    syn.protect_gui(ScreenGui)
 
     Main.Name = "Main"
     Main.Parent = ScreenGui
