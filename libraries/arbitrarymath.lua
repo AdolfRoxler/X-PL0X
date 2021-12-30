65f780fe66076c7b0fc554425933871eaf5a5c20
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2021-12-30 17:00:02 +0100

    update
    Thickness doesn't matter if shit's not hollow and I've written half of the physics library

--- libraries/arbitrarymath.lua
+++ libraries/arbitrarymath.lua
@@ -1,6 +1,6 @@
 meth = {}
 function meth.round(X,decimallimit)
-if decimallimit then else decimallimit = 0 end
+if decimallimit then else decimallimit = 0 end ---found doing this for every time I wanted to round exhausting so here's this piece of shit
 decimalpointer = 10^math.abs(decimallimit)
 return math.round(X*(decimalpointer))/decimalpointer
 end
