8c8ed7b76762c7b69624f2657347bfb4b0c7daa3
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2021-12-30 17:19:26 +0100

    d00d

--- libraries/physicsMANIPULATOR.lua
+++ libraries/physicsMANIPULATOR.lua
@@ -1,40 +1,39 @@
 local Phys = {}
-Phys.PhysQueue = {}
-local PhysQueue = Phys.PhysQueue
+local PhysQueue = {}
 
 function Phys:MoveTo(Inst,Cframe,Aggressivity)
-if Inst:IsA("BasePart") then else return end
-if PhysQueue[Inst] then 
-PhysQueue[Inst].MoveTo = {Cframe,Aggressivity}
-else
-PhysQueue[Inst] = {Obj=Inst,MoveTo={Cframe,Aggressivity}}
-end
+	if Inst:IsA("BasePart") then else return end
+	if PhysQueue[Inst] then 
+		PhysQueue[Inst].MoveTo = {Cframe,Aggressivity}
+	else
+		PhysQueue[Inst] = {Obj=Inst,MoveTo={Cframe,Aggressivity}}
+	end
 end
 function Phys:PurgePhysics(Inst)
-if PhysQueue[Inst] then
-PhysQueue[Inst]=nil
+	if PhysQueue[Inst] then
+		PhysQueue[Inst]=nil
+	end
 end
-end
-function Phys:AlignTo(Inst,CFrame,Aggressivity)
-if Inst:IsA("BasePart") then else return end
-if PhysQueue[Inst] then 
-PhysQueue[Inst].AlignTo = {Cframe,Aggressivity}
-else
-PhysQueue[Inst] = {Obj=Inst,AlignTo={Inst,Cframe,Aggressivity}}
-end
+function Phys:AlignTo(Inst,Cframe,Aggressivity)
+	if Inst:IsA("BasePart") then else return end
+	if PhysQueue[Inst] then 
+		PhysQueue[Inst].AlignTo = {Cframe,Aggressivity}
+	else
+		PhysQueue[Inst] = {Obj=Inst,AlignTo={Inst,Cframe,Aggressivity}}
+	end
 end
 
 game:GetService("RunService").Stepped:connect(function()
-for Obj,_ in pairs(PhysQueue) do
-if _.Obj then if isnetworkowner(_.Obj) then 
-else continue end 
-elseif not _.Obj:IsA("BasePart") then PhysQueue[Obj]=nil continue
-else PhysQueue[Obj]=nil continue end
+	for Obj,_ in pairs(PhysQueue) do
+		if _.Obj then if isnetworkowner(_.Obj) then 
+			else continue end 
+		elseif not _.Obj:IsA("BasePart") then PhysQueue[Obj]=nil continue
+		else PhysQueue[Obj]=nil continue end
 
 
-if _.MoveTo then local vector = _.MoveTo[1].p-_.Obj.CFrame.p _.Obj.AssemblyLinearVelocity = vector*vector.Magnitude*_.MoveTo[2]
-elseif _.AlignTo then end --- not filling in yet
-end
+		if _.MoveTo then local vector = _.MoveTo[1].p-_.Obj.CFrame.p _.Obj.AssemblyLinearVelocity = vector*vector.Magnitude*_.MoveTo[2]
+		elseif _.AlignTo then end --- not filling in yet
+	end
 end)---- This is the thread that does ALL of the work
 
 return Phys
