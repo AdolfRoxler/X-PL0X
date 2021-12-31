82cd26280bff49e4c4e6cb9b2146a544850dda2d
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2021-12-31 12:24:21 +0100

    kys faggot physics poop niggersex

--- libraries/physicsMANIPULATOR.lua
+++ libraries/physicsMANIPULATOR.lua
@@ -4,7 +4,6 @@ local PhysQueue = {}
 function Phys:MoveTo(Inst,Cframe,Aggressivity)
 	if Inst:IsA("BasePart") then else return end
 		PhysQueue[Inst] = {Obj=Inst,MoveTo={Cframe,Aggressivity}}
-	end
 end
 function Phys:PurgePhysics(Inst)
 	if PhysQueue[Inst] then
@@ -14,10 +13,9 @@ end
 function Phys:AlignTo(Inst,Cframe,Aggressivity)
 	if Inst:IsA("BasePart") then else return end
 		PhysQueue[Inst] = {Obj=Inst,AlignTo={Inst,Cframe,Aggressivity}}
-	end
 end
 
-game:GetService("RunService").Stepped:connect(function()
+game:GetService("RunService").Stepped:connect(function(t,DELTA)
 	for Obj,_ in pairs(PhysQueue) do
 		if _.Obj then
 		elseif not _.Obj:IsA("BasePart") then PhysQueue[Obj]=nil continue
@@ -25,8 +23,8 @@ game:GetService("RunService").Stepped:connect(function()
 
 
 		if _.MoveTo then 
-        local vector = _.MoveTo[1]-_.Obj.CFrame.p 
-        _.Obj.AssemblyLinearVelocity = vector*vector.Magnitude*_.MoveTo[2]
+        local vector = _.MoveTo[1].p-_.Obj.CFrame.p 
+        _.Obj.AssemblyLinearVelocity = vector*_.MoveTo[2]
 		elseif _.AlignTo then end --- not filling in yet
 	end
 end)---- This is the thread that does ALL of the work
