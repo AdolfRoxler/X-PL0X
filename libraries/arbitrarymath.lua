4ef0e693cdb553aa13a5d0e00a3231997be28b92
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-01-20 15:12:21 +0100

    remopve ppi

--- libraries/arbitrarymath.lua
+++ libraries/arbitrarymath.lua
@@ -1,5 +1,4 @@
 meth = {}
-meth.pi = math.pi()
 function meth.round(X,decimallimit)
 if decimallimit then else decimallimit = 0 end ---found doing this for every time I wanted to round exhausting so here's this piece of shit
 decimalpointer = 10^math.abs(decimallimit)
