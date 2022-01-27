ce8a9dc871a8ee771c156aa354aeaa90afc69add
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-01-27 22:44:55 +0100

    will fix tomorrow

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -3,7 +3,7 @@ local tamperedmetatable
 spoofer.tamperedinstances = {}
 
 function spoofer:spoof(Inst,prop,val)
-if Inst~=nil and typeof(tostring(prop)) then spoofer.tamperedinstances[Inst] = {Property=prop,Value=val} end
+if Inst~=nil and typeof(prop) then spoofer.tamperedinstances[Inst] = {Property=prop,Value=val} end
 end
 
 function spoofer:unspoof(Inst)
@@ -11,8 +11,8 @@ if spoofer.tamperedinstances[Inst] then spoofer.tamperedinstances[Inst]=nil end
 end
 
 local tamperedmetatable = hookmetamethod(game, "__newindex", newcclosure(function(Self,Index,Value)
-   local index = spoofer.tamperedinstances[Self].Property or Index
-   local value = spoofer.tamperedinstances[Self].Value or Value
+   local index = Index==spoofer.tamperedinstances[Self].Index and spoofer.tamperedinstances[Self].Index or Index
+   local value = Value==spoofer.tamperedinstances[Self].Value and spoofer.tamperedinstances[Self].Value or Value
    return tamperedmetatable(Self,index,value)
 end))
 
