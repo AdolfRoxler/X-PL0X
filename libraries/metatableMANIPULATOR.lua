f07cf4f65a5af3422ddc5a590147bf568019828b
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-01-28 20:59:26 +0100

    bugfix

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -8,12 +8,12 @@ spoofer.tamperedinstances[Inst]={}
 spoofer.tamperedinstances[Inst][Prop]=Val
 end
 function spoofer:unspoof(Inst,Prop)
-if spoofer.tamperedinstances[Inst][Prop] then spoofer.tamperedinstances[Inst][Prop]=nil
+if spoofer.tamperedinstances[Inst] and spoofer.tamperedinstances[Inst][Prop] then spoofer.tamperedinstances[Inst][Prop]=nil
 elseif spoofer.tamperedinstances[Inst] then spoofer.tamperedinstances[Inst]=nil end
 end
 
 spoofer.tamperedmetatable = hookmetamethod(game,"__index",newcclosure(function(Instance,Type)
-if spoofer.tamperedinstances[Instance] and spoofer.tamperedinstances[Instance][Type] then return spoofer.tamperedinstances[Instance][Type],Type end
+if spoofer.tamperedinstances[Instance] and spoofer.tamperedinstances[Instance][Type] then return spoofer.tamperedinstances[Instance][Type] end
 return spoofer.tamperedmetatable(Instance,Type)
 end))
 return spoofer
