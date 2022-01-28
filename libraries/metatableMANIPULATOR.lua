2292eddae6b88b390d7aa664c6da53a68c5e7d33
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-01-28 09:03:34 +0100

    dunno if this shit works

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -1,24 +1,20 @@
 local spoofer = {}
 spoofer.tamperedmetatable = nil
-local tamperedmetatable = spoofer.tamperedmetatable
-spoofer.tamperedproperties = {}
+spoofer.tamperedinstances =  {}
 
-function spoofer:spoof(Inst,prop,val)
-if Inst~=nil and typeof(prop) then spoofer.tamperedproperties[prop] = spoofer.tamperedproperties[prop] or {} spoofer.tamperedproperties[prop][Inst]=val end
-end
-
-function spoofer:unspoof(Inst,prop)
-if spoofer.tamperedproperties[prop] and spoofer.tamperedproperties[prop][Inst] then spoofer.tamperedinstances[prop][Inst]=nil 
-elseif typeof(Inst)=="Instance" and not spoofer.tamperedproperties[prop] then  
-for ref,val in pairs(spoofer.tamperedproperties) do
-if ref[Inst] then ref[Inst]=nil end
-end
-end
+function spoofer:spoof(Inst,Prop,Val)
+spoofer.tamperedinstances[Inst][Prop]=Val
 end
 
-local tamperedmetatable = hookmetamethod(game, "__index", newcclosure(function(Prop,Type)
-   local value = spoofer.tamperedproperties[Prop][] or Value
-   return tamperedmetatable(Self,value)
+function spoofer:unspoof(Inst,Prop)
+spoofer.tamperedinstances[Inst][Prop]=nil
+end
+
+spoofer.tamperedmetatable = hookmetamethod(game,"__index",newcclosure(function(Property,Type)
+for INSTANCE in pairs(spoofer.tamperedinstances) do
+if INSTANCE[Property] then return INSTANCE[Property],Type
+end
+return spoofer.tamperedmetatable(property,Type)
 end))
 
 return spoofer
