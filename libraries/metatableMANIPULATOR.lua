b5127bddd79520037fc1326db1a7b3b91a7d4472
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-01-28 09:15:31 +0100

    This should work
    ..but i didn't test it

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -5,16 +5,12 @@ spoofer.tamperedinstances =  {}
 function spoofer:spoof(Inst,Prop,Val)
 spoofer.tamperedinstances[Inst][Prop]=Val
 end
-
 function spoofer:unspoof(Inst,Prop)
 spoofer.tamperedinstances[Inst][Prop]=nil
 end
 
-spoofer.tamperedmetatable = hookmetamethod(game,"__index",newcclosure(function(Property,Type)
-for INSTANCE in pairs(spoofer.tamperedinstances) do
-if INSTANCE[Type]==Property then return spoofer.tamperedmetatable(INSTANCE[Property],Type)
-end
+spoofer.tamperedmetatable = hookmetamethod(game,"__index",newcclosure(function(Instance,Type)
+if spoofer.tamperedinstances[Instance] and spoofer.tamperedinstances[Instance][Type] then return spoofer.tamperedinstances[Instance][Type],Type
 return spoofer.tamperedmetatable(property,Type)
 end))
-
 return spoofer
