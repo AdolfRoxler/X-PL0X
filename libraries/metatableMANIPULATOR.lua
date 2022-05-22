dc63a079574055c9fe70ec7f70fbdf9add29e6fe
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-05-22 18:47:01 +0200

    updat

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -15,10 +15,11 @@ if spoofer.tamperedinstances[Inst] and spoofer.tamperedinstances[Inst][Prop] the
 elseif spoofer.tamperedinstances[Inst] then spoofer.tamperedinstances[Inst]=nil end
 end
 
-function spoofer:spooffunction(Inst,Function,Args)
+function spoofer:spooffunction(Inst,Function,Args,Replacement)
 if Inst and Function and Args then else return end
 if spoofer.tamperedfunctions[Inst]==nil then spoofer.tamperedfunctions[Inst]={} end
-spoofer.tamperedfunctions[Inst][tostring(Function)] = Args
+spoofer.tamperedfunctions[Inst][tostring(Function)].Target = Args
+spoofer.tamperedfunctions[Inst][tostring(Function)].Replace = Replacement
 end
 
 spoofer.tamperedmetatable = hookmetamethod(game,"__index",newcclosure(function(Instance,Type)
@@ -29,8 +30,8 @@ end))
 spoofer.namecall = hookmetamethod(game, "__namecall", function(Self, ...)
 local arguments = {...}
 local method = getnamecallmethod()
-if not checkcaller() and spoofer.tamperedfunctions[Self]~=nil and spoofer.tamperedfunctions[Self][Function]~=nil then
-return spoofer.tamperedfunctions[Self][Function]
+if not checkcaller() and spoofer.tamperedfunctions[Self]~=nil and spoofer.tamperedfunctions[Self][method]~=nil and spoofer.tamperedfunctions[Self][method].Replacement~=nil then
+return spoofer.tamperedfunctions[Self][method].Target~=nil and spoofer.tamperedfunctions[Self][method].Target==arguments and spoofer.tamperedfunctions[Self][method].Replacement or spoofer.tamperedfunctions[Self][method].Replacement~=nil and spoofer.tamperedfunctions[Self][method].Replacement or arguments
 end
 end)
 
