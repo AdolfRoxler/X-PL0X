a7d9dd647797608ca4a5fb124a3243e84f0f3d16
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-05-23 18:55:47 +0200

    sex scripting

--- libraries/metatableMANIPULATOR.lua
+++ libraries/metatableMANIPULATOR.lua
@@ -36,11 +36,12 @@ end
 
 function spoofer:probefunction(Inst,Function,ignoresyn)
 if Function then else return end
-local ignoresyn = ignoresyn
 if typeof(Inst)=="Instance" then else Inst=false end
-if type(ignoresyn)=="boolean" then else ignoresyn=false end
+local ignoresyn = ignoresyn
+if type(ignoresyn)=="boolean" then else ignoresyn=true end
 if spoofer.dumpedfunctions[Inst]==nil then spoofer.dumpedfunctions[Inst]={} end
-spoofer.dumpedfunctions[Inst][Function]=true
+if spoofer.dumpedfunctions[Inst][Function]==nil then spoofer.dumpedfunctions[Inst][Function]={} end
+spoofer.dumpedfunctions[Inst][Function].engaged = true
 spoofer.dumpedfunctions[Inst][Function].ignoresyn = ignoresyn
 end
 
@@ -64,7 +65,7 @@ local method = getnamecallmethod()
 
 local Self2 = spoofer.dumpedfunctions[Self]~=nil and Self or false
 
-if spoofer.dumpedfunctions[Self2]~=nil and spoofer.dumpedfunctions[Self2][method]==true and ((spoofer.dumpedfunctions[Self2].ignoresyn==true and not syncall) or spoofer.dumpedfunctions[Self2].ignoresyn==false) then 
+if spoofer.dumpedfunctions[Self2]~=nil and spoofer.dumpedfunctions[Self2][method].engaged==true and ((spoofer.dumpedfunctions[Self2][method].ignoresyn==true and syncall~=true) or spoofer.dumpedfunctions[Self2][method].ignoresyn==false) then 
 --local args = type(...)=="table" and tostring(table.unpack(...)) or tostring(...)
 --print(args)
 print(tostring(method)..':\n{ \n Self: '..tostring(Self).." \n Arguments: "..tostring(...).." \n}")
@@ -76,7 +77,7 @@ end
 Self2 = spoofer.tamperedfunctions[Self]~=nil and Self or false
 
 if spoofer.tamperedfunctions[Self2] and spoofer.tamperedfunctions[Self2][method] and spoofer.tamperedfunctions[Self2][method].Replacement then
-if spoofer.tamperedfunctions[Self2][method].ignoresyn==true and syncall then return spoofer.namecall(Self,...) end
+if spoofer.tamperedfunctions[Self2][method].ignoresyn==true and syncall==true then return spoofer.namecall(Self,...) end
 --if spoofer.tamperedfunctions[Self][method].Target~=nil and spoofer.tamperedfunctions[Self][method].Target==arguments then return spoofer.namecall(Self,spoofer.tamperedfunctions[Self][method].Replacement) elseif spoofer.tamperedfunctions[Self][method].Target==nil then return spoofer.namecall(Self,spoofer.tamperedfunctions[Self][method].Replacement) end end
 --return spoofer.tamperedfunctions[Self][method].Replacement
 --if spoofer.tamperedfunctions[Self][method].Target == ... or spoofer.tamperedfunctions[Self][method].Target == nil then
@@ -91,10 +92,12 @@ end
 --print(...)
 return spoofer.namecall(Self,...)
 end)
-        --[[spoofer:dumpfunction(workspace,"FindPartOnRayWithIgnoreList") --- These are examples on dumping
+        --[[spoofer:dumpfunction(workspace,"FindPartOnRayWithIgnoreList") --- These are dump function examples
         spoofer:dumpfunction(workspace,"FindPartOnRayWithWhitelist")
         spoofer:dumpfunction(workspace,"FindPartOnRay")
         spoofer:dumpfunction(workspace,"findPartOnRay")
         spoofer:dumpfunction(workspace,"Raycast")]] --- cba to do docs figure out urself fucking skid!
+
+--I bet my ass this can be even more optimized lol
 
 return spoofer
