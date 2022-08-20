c6bf0a1eec10bebf36529e1eef5c59018b25b341
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-08-20 23:48:23 +0200

    sex

--- libraries/Unused/drawinglib.lua
+++ libraries/Unused/drawinglib.lua
@@ -3,22 +3,22 @@ local User = game:GetService('Players').LocalPlayer
 game:GetService("RunService").RenderStepped:connect(function() -- Constant updater to ease up resource use
     Camera = workspace.CurrentCamera
     Mouse = User:GetMouse()
-    Resolution = Vector2.new(Mouse.ViewSizeX,Mouse.ViewSizeY)
+    Resolution = Camera.ViewportSize
 end)
 ------------- DO NOT USE THIS SHIT
 local GraphicalHierarchy = {}
 
 local functions = {}
 
-function functions:MakeBox(Pos,Size,Filled,BorderThickness,Color) --- Scaling based on vertical res because it's better. Argue with the retards that stretch like the guys on WOS and make ultrawide unplayable
+function functions:CreateFrame(Pos,Size,Filled,BorderThickness,Color) --- Scaling based on vertical res because it's better. Argue with the retards that stretch like the guys on WOS and make ultrawide unplayable
 if Pos and Size then else return nil end
 
 local Frame = Drawing.new("Quad")
 
-local PX = Pos.X.Scale*workspace.CurrentCamera.ViewportSize.X+Pos.X.Offset      -- this of course should not depend on vertical res
-local PY = Pos.Y.Scale*workspace.CurrentCamera.ViewportSize.Y+Pos.Y.Offset
-local SizeX = Size.X.Scale*workspace.CurrentCamera.ViewportSize.Y+Size.X.Offset
-local SizeY = Size.Y.Scale*workspace.CurrentCamera.ViewportSize.Y+Size.Y.Offset
+local PX = Pos.X.Scale*Resolution.X+Pos.X.Offset      -- this of course should not depend on vertical res
+local PY = Pos.Y.Scale*Resolution.Y+Pos.Y.Offset
+local SizeX = Size.X.Scale*Resolution.Y+Size.X.Offset
+local SizeY = Size.Y.Scale*Resolution.Y+Size.Y.Offset
 
 Frame.PointB = Vector2.new(PX,PY)
 Frame.PointA = Vector2.new(PX+SizeX,PY)
