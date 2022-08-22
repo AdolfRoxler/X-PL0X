d1abe904a4bc2dccdf9d5ead0b183bfa543af8de
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-08-22 12:57:36 +0200

    temp fix

--- libraries/Unused/drawinglib.lua
+++ libraries/Unused/drawinglib.lua
@@ -1,18 +1,16 @@
 if not syn then warn("bro nigga chill out I didn't make this for other gaysploits yet") else repeat wait() until syn end
 local User = game:GetService('Players').LocalPlayer
-game:GetService("RunService").RenderStepped:connect(function() -- Constant updater to ease up resource use
+game:GetService("RunService").RenderStepped:connect(function() -- Im lazy ok?
     Camera = workspace.CurrentCamera
     Mouse = User:GetMouse()
     Resolution = Camera.ViewportSize
 end)
-------------- DO NOT USE THIS SHIT
 local GraphicalHierarchy = {}
 
 local functions = {}
 
 function functions:CreateFrame(Pos,Size,Filled,BorderThickness,Color) --- Scaling based on vertical res because it's better. Argue with the retards that stretch like the guys on WOS and make ultrawide unplayable
 if Pos and Size then else return nil end
-
 local Frame = Drawing.new("Quad")
 
 local PX = Pos.X.Scale*Resolution.X+Pos.X.Offset      -- this of course should not depend on vertical res
@@ -28,16 +26,10 @@ Frame.PointD = Vector2.new(PX+SizeX,PY+SizeY)
 if type(Filled)=="boolean" then
 Frame.Filled = Filled 
 end
-
 Frame.Color = Color
-
 Frame.Visible = true
-
-
 Frame.Thickness = tonumber(BorderThickness)
-
 table.insert(GraphicalHierarchy,Frame)
-
 return Frame
 end
 
