afa2c8c59514b2685205704528aa93a04f9628c5
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-10-14 07:07:40 +0200

    fucking commas!

--- libraries/Config.lua
+++ libraries/Config.lua
@@ -2,13 +2,13 @@ return {
 ESP = {
     Disabled = false, Box = false, Healthbar = false, Tracers = false, Head = false, 
     Chams = {Disabled = true, ThroughWalls = false},
-    OrientateBox = false -- Orient it to character rotation
+    OrientateBox = false, -- Orient it to character rotation
     Nametag = {Disabled = true, DisplayName = false, DisplayHealth = false, DisplayAvatar = false, DisplayDistance = false,
-    Customization = {
-       Base = {Color = Color3.new(0,0,0), Opacity=1},
-       SecondaryLeft = {Color = Color3.new(0,0,0), Opacity=1},
-       SecondaryRight = {Color = Color3.new(0,0,0), Opacity=1},
-     },
+        Customization = {
+        Base = {Color = Color3.new(0,0,0), Opacity=1},
+        SecondaryLeft = {Color = Color3.new(0,0,0), Opacity=1},
+        SecondaryRight = {Color = Color3.new(0,0,0), Opacity=1},
+        },
     },
-
+   }
 }
