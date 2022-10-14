19a5dcac5d459a615836d9f845427c366f9dc331
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2022-10-14 06:36:05 +0200

    moar configz cuh!

--- libraries/Config.lua
+++ libraries/Config.lua
@@ -1,4 +1,12 @@
 return {
-ESP = false,
-
+ESP = {
+    Disabled = false, Box = false, Healthbar = false, Tracer = false, 
+    Chams = {Disabled = true, ThroughWalls = false},
+    Nametag = {DisplayName = false, DisplayHealth = false, DisplayAvatar = false, DisplayDistance = false,
+    Customization = {
+       Base = {Color = Color3.new(0,0,0), Opacity=1},
+       SecondaryLeft = {Color = Color3.new(0,0,0), Opacity=1},
+       SecondaryRight = {Color = Color3.new(0,0,0), Opacity=1},
+     },
+    },
 }
