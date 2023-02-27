ecb0412d205ff7972fb7a4b23fd152198c1bf3b9
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2023-02-27 16:23:01 +0100

    true lowercase cuh

--- libraries/Config.lua
+++ libraries/Config.lua
@@ -3,7 +3,7 @@ return {
         enabled = false, tracers = false, head = false, 
         chams = {enabled = false, throughWalls = false},
         box = {enabled = false, healthbar = false, dynamic = false},
-        nametag = {enabled = false, displayName = false, displayHealth = false, displayAvatar = false, displayDistance = false,
+        nametag = {enabled = false, displayname = false, displayhealth = false, displayavatar = false, displaydistance = false,
             customization = {
                 base = {color = Color3.new(0,0,0), opacity=1},
                 secondaryLeft = {color = Color3.new(0,0,0), opacity=1},
