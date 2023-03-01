009410cee3b8a2a1760d2139c5d7d622952df552
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2023-03-01 19:42:49 +0100

    Try to make precision mode in that context a bit more performant

--- libraries/X-API.lua
+++ libraries/X-API.lua
@@ -61,13 +61,13 @@ local floor = math.floor
 local split = string.split
 local tostring = tostring
 local tonumber = tonumber
-local lshift = function(a,b,p) return p and a*(2^b) or bit.lshift(a,b) end
-local rshift = function(a,b,p) return p and a*(2^-b) or bit.rshift(a,b) end
+local lshift = function(a,b,p) return p and a*(1*2*b) or bit.lshift(a,b) end
+local rshift = function(a,b,p) return p and a*(1/(2*b)) or bit.rshift(a,b) end
 	--local lshift = bit and Config.esp.precise and bit.lshift or and function(a,b) return a*(2^b) end
 	---
 
 	local function scaletotextbound(Text,Bounds,u)
-		if Text.TextBounds.XB>ounds.X or Text.TextBounds.Y>Bounds.Y then repeat Text.Size -= u until Text.TextBounds.X == Bounds.X and Text.TextBounds.Y == Bounds.Y 
+		if Text.TextBounds.XB>Bounds.X or Text.TextBounds.Y>Bounds.Y then repeat Text.Size -= u until Text.TextBounds.X == Bounds.X and Text.TextBounds.Y == Bounds.Y 
 		elseif Text.TextBounds.X<Bounds.X or Text.TextBounds.Y<Bounds.Y then repeat Text.Size += u until Text.TextBounds.X == Bounds.X and Text.TextBounds.Y == Bounds.Y end 
 	end
 
