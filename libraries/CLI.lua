53705141615020bb9030562f1e363dce5e64d280
AdolfRoxler <91347019+AdolfRoxler@users.noreply.github.com>
2023-03-05 12:27:34 +0000

    hotfix
    i really need a clean output on linux lol

--- libraries/CLI.lua
+++ libraries/CLI.lua
@@ -4,8 +4,7 @@ rconsolename("X-CLI")
 function API:DisplayText(Text: string, Color: string)
 	if Text and Text~="" and Text~="\n" then
 	Color = Color~=nil and Color or ""
-	if syn then Color = Color~="" and "@@"..Color.."@@" else Color = Color~="" and string.lower(Color) end
-	
+	if syn then Color = Color~="" and "@@"..Color.."@@" else end
 	--local Color = Color and Color~="" and (syn and ("@@"..Color.."@@" or "@@WHITE@@") or (white"))
 	if syn then
 	rconsoleprint(Color)
@@ -18,9 +17,9 @@ end
 
 function API:Prompt(Prompt: string, Color: string, Callback)
 	API:DisplayText(Prompt.."\n",Color)
-	rconsoleprint(syn and "@@LIGHT_GREEN@@" or "bgreen")
+	rconsoleprint(syn and "@@LIGHT_GREEN@@" or "")
     rconsoleprint("["..game:GetService("Players").LocalPlayer.Name.."@"..game:GetService("Players").LocalPlayer.DisplayName.."]$ ")
-	rconsoleprint(syn and "@@WHITE@@" or "white")
+	rconsoleprint(syn and "@@WHITE@@" or "")
 	local Callback = Callback or function() end
 	pcall(function() Callback(rconsoleinput()) end)
 end
