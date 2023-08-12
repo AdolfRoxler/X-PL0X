efb4e524eb02e7fb2731dbcd9c0164c0d861d84c
Joe Biden <fakemail@fakeasfuck.cum>
2023-08-12 18:55:14 +0200

    whoops :)

--- libraries/X-API.lua
+++ libraries/X-API.lua
@@ -19,7 +19,7 @@ local PlayerList = {}
 local Random = Random.new(tick())
 local time = os.clock
 local Draw = Drawing.new
---local SafeFolder = Instance.new("Folder",game.CoreGui) SafeFolder.Name = "GhettoSmosh"
+local SafeFolder = Instance.new("Folder",game.CoreGui) SafeFolder.Name = "GhettoSmosh"
 local lowvalue = -(2^31-1)
 local AvatarURL = "https://www.roblox.com/headshot-thumbnail/image?userId=ñ&width=512&height=512&format=png"
 --syn.protect_gui(SafeFolder)
