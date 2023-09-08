f3269cbfd7c3d4e5350f85b43dc33f64eebe1378
Joe Biden <fakemail@fakeasfuck.cum>
2023-09-08 20:31:17 +0200

    oops

--- libraries/Config.lua
+++ libraries/Config.lua
@@ -1,7 +1,7 @@
 return {
     render = {
         esp = {
-            enabled = true, precise = false, head = false, ignorecorpses = true, skeleton = false -- If precise is set to false it'll use bitwise operations, which are WAY FASTER.
+            enabled = true, precise = false, head = false, ignorecorpses = true, skeleton = false, -- If precise is set to false it'll use bitwise operations, which are WAY FASTER.
             chams = {enabled = false, throughwalls = false},  -- purged due to limitation of 31 objects. NOT GOOD.
             box = {enabled = true, healthbar = false, dynamic = false},
             tracers = {enabled = false, maxdistance = 200},
